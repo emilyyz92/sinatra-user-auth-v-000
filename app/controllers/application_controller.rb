1aa9ac9aedd7064d8ef2cd3324655b866c41c891
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-27 23:48:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
     session[:user_id] = @user.id
     redirect '/users/home'
   end
