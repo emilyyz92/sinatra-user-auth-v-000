c86e27ce1bff6d52d115acc8c8574a7678e2b219
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-28 00:36:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,13 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
-    session[:user_id] = @user.id
-    redirect '/users/home'
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/sessions/login'
+    end
+
   end
 
   get '/sessions/logout' do
